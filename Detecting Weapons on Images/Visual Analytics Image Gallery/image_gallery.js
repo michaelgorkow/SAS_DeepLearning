50a415840de164d59708b7fc7709b3444b67b4b0
germgk <michael.gorkow@sas.com>
2020-05-13 13:23:56 +0200

    added weapon detection example

--- Detecting Weapons on Images/Visual Analytics Image Gallery/image_gallery.js
+++ Detecting Weapons on Images/Visual Analytics Image Gallery/image_gallery.js
@@ -1,26 +1,37 @@
-var myPhoto = ["https://www.welt.de/img/geschichte/mobile132257365/8642507307-ci102l-w1024/Forscher-geben-den-Ahnen-des-Menschen-ein-Gesicht.jpg", "https://www.versicherungsmagazin.de/sixcms/media.php/1919/thumbnails/Anke%20Nienkerke-Springer_4c.jpg.24261580.jpg", "https://www.giessener-allgemeine.de/bilder/2019/12/01/13262596/699330945-meinung_hintergrund_4c-a_4c_3-3qac.jpg", "https://cdn.prod.www.spiegel.de/images/df112fa2-0001-0004-0000-000000089622_w948_r1.77_fpx43.9_fpy50.jpg", "https://cdn.mdr.de/wissen/mensch-alltag/maschinemensch-102-resimage_v-variantBig24x9_w-1024.jpg?version=490"];
+if (window.addEventListener) {
+   // For standards-compliant web browsers
+   window.addEventListener("message", onMessage, false);
+} else {
+   window.attachEvent("onmessage", onMessage);
+}
+   // Retrieve data and begin processing
+function onMessage(event) {
+   if (event && event.data) {
+   //process event.data
+   var container = document.getElementById("table_container");
+   container.innerHTML="";
+   var table = document.createElement("table");
+   document.getElementById("table_container").appendChild(table);
 
-var container = document.getElementById("table_container");
-
-var table = document.createElement("table");
-document.getElementById("table_container").appendChild(table);
-
-var row = document.createElement("tr");
-for (var i=0, len = myPhoto.length; i < len; ++i) {
-    var photo =  document.createElement("td");
-    img = new Image();
-    img.src = myPhoto[i];
-    img.height = 224;
-    img.width = 224;
-    photo.appendChild(img);
-    row.appendChild(photo);
-    if (i % 2 == 0 && i > 0) {
-       table.appendChild(row);
-       row = document.createElement("tr");
-    }
-    if (i == myPhoto.length -1 ) {
-       console.log('end');
-       table.appendChild(row);
+   var row = document.createElement("tr");
+   for (var i=0, len = event.data.availableRowCount; i < len; ++i) {
+       var photo =  document.createElement("td");
+       img = new Image();
+       img.src = event.data.data[i][0];
+       img.height = event.data.data[i][1];
+       img.width = event.data.data[i][1];
+       photo.appendChild(img);
+       row.appendChild(photo);
+       console.log(i+1 % 3);
+       if ((i+1) % 3 == 0 && i > 0) {
+          table.appendChild(row);
+          row = document.createElement("tr");
+       }
+       console.log(i, event.data.availableRowCount);
+       if (i == event.data.availableRowCount-1) {
+          console.log('end');
+          table.appendChild(row);
     }
 }
-
+   }
+}
